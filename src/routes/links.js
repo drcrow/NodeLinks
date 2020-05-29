4fe8c009ad79befc7c3b6086ffd50d0a7435bd0b
Agustin Fiori <afiori@clarika.com.ar>
2020-05-29 17:39:21 -0300

    Restricted Routes

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const {isLoggedIn} = require('../lib/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', isLoggedIn, async (req, res) => {
     const links = await pool.query('SELECT * FROM links');
     res.render('links/list', {
         links: links
     });
 });
 
-router.get('/add', (req, res) => {
+router.get('/add', isLoggedIn, (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', async(req, res) => {
+router.post('/add', isLoggedIn, async (req, res) => {
     // podríamos usar directamente req.body pero en este caso creamos un nuevo objeto newLink para contener los datos
     const { title, url, description } = req.body;
     const newLink = {
@@ -27,7 +28,7 @@ router.post('/add', async(req, res) => {
     res.redirect('/links');
 });
 
-router.get('/delete/:id', async(req, res) => {
+router.get('/delete/:id', isLoggedIn, async (req, res) => {
     //console.log(req.params.id);
     const {id} = req.params;
     await pool.query('DELETE FROM links WHERE id = ?', [id]);
@@ -35,7 +36,7 @@ router.get('/delete/:id', async(req, res) => {
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async(req, res) => {
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
     //console.log(req.params.id);
     const {id} = req.params;
     const link = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
@@ -45,7 +46,7 @@ router.get('/edit/:id', async(req, res) => {
     });
 });
 
-router.post('/edit', async(req, res) => {
+router.post('/edit', isLoggedIn, async (req, res) => {
     await pool.query('UPDATE links SET title = ?, url = ?, description = ? WHERE id = ?', [req.body.title, req.body.url, req.body.description, req.body.id]);
     req.flash('success', 'The Link whas edited');
     res.redirect('/links');
